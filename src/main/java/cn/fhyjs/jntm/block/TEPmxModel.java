d31f83052dcad67fdf3fad57a9ee383af70e7d25
fhyjs <fenhyjs@163.com>
2023-07-31 16:16:47 +0800

    mmdRender

--- src/main/java/cn/fhyjs/jntm/block/TEPmxModel.java
+++ src/main/java/cn/fhyjs/jntm/block/TEPmxModel.java
@@ -2,23 +2,33 @@ package cn.fhyjs.jntm.block;
 
 
 import cn.fhyjs.jntm.interfaces.IhasMMDRender;
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
+import li.cil.oc.api.network.SimpleComponent;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.play.server.SPacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ITickable;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.Optional;
 import org.jetbrains.annotations.NotNull;
-
-public class TEPmxModel extends TileEntity implements ITickable, IhasMMDRender {
+@Optional.Interface(iface = "li.cil.oc.api.network.SimpleComponent", modid = "opencomputers")
+public class TEPmxModel extends TileEntity implements ITickable, IhasMMDRender, SimpleComponent {
     public String modelName,idleActionName,redStoneActionName,currentActionName="idle";
+    public boolean oldstate;
     @Override
     public void update() {
         TileEntity te = world.getTileEntity(pos);
         if(te instanceof TEPmxModel){
-            if (world.isBlockPowered(pos)) {
-                this.currentActionName=redStoneActionName;
-            }else {
-                this.currentActionName=idleActionName;
+            if (world.isBlockPowered(pos)!=oldstate) {
+                oldstate=world.isBlockPowered(pos);
+                if (oldstate){
+                    this.currentActionName=redStoneActionName;
+                }else {
+                    this.currentActionName=idleActionName;
+                }
             }
         }
     }
@@ -69,4 +79,14 @@ public class TEPmxModel extends TileEntity implements ITickable, IhasMMDRender {
     public String getActionName() {
         return currentActionName;
     }
+    @Override
+    public String getComponentName() {
+        return "fancy_thing";
+    }
+    @Callback
+    @Optional.Method(modid = "opencomputers")
+    public Object[] greet(Context context, Arguments args) {
+        return new Object[]{String.format("Hello, %s!", args.checkString(0))};
+    }
+
 }
